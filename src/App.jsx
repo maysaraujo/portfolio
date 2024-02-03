ebade0a0bdeda8bcd98ee27798a454bcffde2c66
maysaraujo <maysaaraujo25@gmail.com>
2024-02-03 20:51:26 -0300

    feat: adding a experience component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import About from './components/About/About';
 import Apresentation from './components/Apresentation/Apresentation';
 import Experience from './components/Experience/Experience';
 import Header from './components/Header/Header';
+import Skills from './components/Skills/Skills';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Apresentation />
       <About />
       <Experience />
+      <Skills />
     </div>
   );
 }
