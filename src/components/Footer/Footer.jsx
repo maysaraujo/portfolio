f59c3aa5d9324e2c1f25039cd1c0db7b8b10d991
maysaraujo <maysaaraujo25@gmail.com>
2024-03-15 12:11:51 -0300

    feat: adding current year function to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import './Footer.module.css';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer>
       <div className='container'>
         <p>
-          Copyright © 2024 <span>Maysa Araujo.</span> Todos os direitos
+          Copyright © {currentYear} <span>Maysa Araujo.</span> Todos os direitos
           reservados.{' '}
         </p>
       </div>
