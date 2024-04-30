12c9888549b193ad935d50cc636c341a3dee9239
maysaraujo <maysaaraujo25@gmail.com>
2024-04-29 22:07:37 -0300

    removendo o useRef e adicionando o navlink aos links do menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,10 @@ import styles from './Header.module.css';
 import { useState } from 'react';
 import { FaTimes } from 'react-icons/fa';
 import { HiMiniBars3 } from 'react-icons/hi2';
+import { NavHashLink as NavLink } from 'react-router-hash-link';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const Header = ({
-  aboutRef,
-  apresentationRef,
-  experienceRef,
-  skillsRef,
-  projectRef,
-  contactRef,
-}) => {
+const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const showMenu = () => {
@@ -22,59 +17,41 @@ const Header = ({
     <header>
       <div className='container'>
         <nav>
-          <ul>
-            <div className={styles.dev}>
-              <li
-                onClick={() =>
-                  apresentationRef.current.scrollIntoView({
-                    behavior: 'smooth',
-                  })
-                }
+          <Router>
+            <ul>
+              <div className={styles.dev}>
+                <NavLink smooth to='#apresentacao'>
+                  <li>Maysa.dev</li>
+                </NavLink>
+              </div>
+              <button className={styles.menuBtn} onClick={showMenu}>
+                {isOpen ? <FaTimes /> : <HiMiniBars3 />}
+              </button>
+              <div
+                className={`${styles.menu} ${isOpen ? styles.showMenu : ''}`}
               >
-                Maysa.dev
-              </li>
-            </div>
-            <button className={styles.menuBtn} onClick={showMenu}>
-              {isOpen ? <FaTimes /> : <HiMiniBars3 />}
-            </button>
-            <div className={`${styles.menu} ${isOpen ? styles.showMenu : ''}`}>
-              <li
-                onClick={() =>
-                  aboutRef.current.scrollIntoView({ behavior: 'smooth' })
-                }
-              >
-                Sobre mim
-              </li>
-              <li
-                onClick={() =>
-                  experienceRef.current.scrollIntoView({ behavior: 'smooth' })
-                }
-              >
-                Experiências
-              </li>
-              <li
-                onClick={() =>
-                  skillsRef.current.scrollIntoView({ behavior: 'smooth' })
-                }
-              >
-                Habilidades
-              </li>
-              <li
-                onClick={() =>
-                  projectRef.current.scrollIntoView({ behavior: 'smooth' })
-                }
-              >
-                Projetos
-              </li>
-              <li
-                onClick={() =>
-                  contactRef.current.scrollIntoView({ behavior: 'smooth' })
-                }
-              >
-                Contato
-              </li>
-            </div>
-          </ul>
+                <NavLink smooth to='#sobre'>
+                  <li>Sobre mim</li>
+                </NavLink>
+
+                <NavLink smooth to='#experiencias'>
+                  <li>Experiências</li>
+                </NavLink>
+
+                <NavLink smooth to='#habilidades'>
+                  <li>Habilidades</li>
+                </NavLink>
+
+                <NavLink smooth to='#projetos'>
+                  <li>Projetos</li>
+                </NavLink>
+
+                <NavLink smooth to='#contato'>
+                  <li>Contato</li>
+                </NavLink>
+              </div>
+            </ul>
+          </Router>
         </nav>
       </div>
     </header>
